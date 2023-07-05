c55fadc64bb3101bcb85e0192516adae3aea7706
umarenjiner <umarnormamatov.06@gmail.com>
2023-07-05 05:41:24 -0700

    User entity

---
+++ src/main/java/io/realworld/angular/conduit/controller/model/User.java
@@ -0,0 +1,34 @@
+package io.realworld.angular.conduit.controller.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String  email;
+
+    @Column(nullable = false)
+    private String userName;
+    private String bio;
+    private String imageUrl;
+    @Column(nullable = false)
+    private  String password;
+
+   /* @OneToMany(fetch = FetchType.LAZY, mappedBy = "user_id")
+    private List<Article> articles;
+*/
+}
