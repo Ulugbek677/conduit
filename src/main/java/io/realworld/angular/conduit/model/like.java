84e1e22222d57f0fb61d8b1cd9ff5cc3123b4425
usmanov0 <azikusmanov882@gmail.com>
2023-07-05 17:41:44 +0500

    LikeEntity

---
+++ src/main/java/io/realworld/angular/conduit/model/like.java
@@ -0,0 +1,14 @@
+package io.realworld.angular.conduit.model;
+
+
+import jakarta.persistence.Entity;
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class like {
+
+}
