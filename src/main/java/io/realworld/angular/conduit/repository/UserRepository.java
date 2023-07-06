269b600f254872e24322fc358d07908d4946bfab
Tuvolov Ulugbek <tuvalovulugbek677@gmail.com>
2023-07-06 19:22:59 +0500

    following

---
+++ src/main/java/io/realworld/angular/conduit/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package io.realworld.angular.conduit.repository;
+
+import io.realworld.angular.conduit.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "select case when count(*) > 0 then true else false end" +
+            "from Article a join follows f" +
+            "on a.author_id = f.follower_id" +
+            "where a.author_id = ? and f.follower_id = ?", nativeQuery = true)
+    Boolean isFollowedToArticleOwner(Long articleId,Long userId);
+}
--- src/main/java/io/realworld/angular/conduit/service/impl/ArticleServiceImpl.java
+++ src/main/java/io/realworld/angular/conduit/service/impl/ArticleServiceImpl.java
@@ -1,10 +1,12 @@
-package io.realworld.angular.conduit.service;
+package io.realworld.angular.conduit.service.impl;
 
 import io.realworld.angular.conduit.dto.ArticleDTO;
 import io.realworld.angular.conduit.dto.response.ArticleResponse;
 import io.realworld.angular.conduit.mapper.ArticleMapper;
 import io.realworld.angular.conduit.model.Article;
 import io.realworld.angular.conduit.repository.ArticleRepository;
+import io.realworld.angular.conduit.repository.UserRepository;
+import io.realworld.angular.conduit.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ArticleServiceImpl implements ArticleService {
     private final ArticleRepository articleRepository;
+    private  final UserRepository userRepository;
 
     @Override
     public ResponseEntity<ArticleResponse> getArticle(String slug) {
@@ -28,9 +31,11 @@ public class ArticleServiceImpl implements ArticleService {
         }
         //TODO change userId
         boolean favorited = articleRepository.isFavorited(0L, id);
+        Boolean following = userRepository.isFollowedToArticleOwner(0L, id);
         long favoritesCount = articleRepository.getFavoritesCount(id);
         Article article = optionalArticle.get();
         ArticleDTO articleDTO = ArticleMapper.toDto(article);
+        articleDTO.getAuthor().setFollowing(following);
         articleDTO.setFavorited(favorited);
         articleDTO.setFavoritesCount(favoritesCount);
         return ResponseEntity.ok(new ArticleResponse(articleDTO));
